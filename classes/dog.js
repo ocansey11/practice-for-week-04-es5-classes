a67e9431fd9cd8bfe5aa8e9a138ccd84a00eb7bb
kevin ocansey <kevinocansey11@gmail.com>
2022-12-21 21:49:42 +0000

    refactored dog class to Es5 class

--- classes/dog.js
+++ classes/dog.js
@@ -1,18 +1,34 @@
-class Dog {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+// class Dog {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
 
-  bark() {
-    console.log("Bark");
-  }
+//   bark() {
+//     console.log("Bark");
+//   }
 
-  static diet() {
-    return "carnivore";
-  }
-}
+//   static diet() {
+//     return "carnivore";
+//   }
+// }
 
+
+
+// Refactored Dog class to Es5 Class
+
+function Dog(name, age){
+  this.name = name
+  this.age = age
+ }
+
+ Dog.prototype.bark = function(){
+  console.log("Bark");
+ }
+
+ Dog.diet = function(){
+  return "carnivore";
+ }
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
