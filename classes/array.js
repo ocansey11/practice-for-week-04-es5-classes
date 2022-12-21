13e94197169c320ba766885b58119c1449b6dfba
kevin ocansey <kevinocansey11@gmail.com>
2022-12-21 21:40:49 +0000

    installed npm and passed all tests for array-spec.js

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,22 @@
 // Your code here
+Array.prototype.isEqual =  function(array){
+
+
+    if(array.length === this.length){
+        let countEqual  = 0
+        for(let i = 0; i < array.length;i++){
+            let value = array[i]
+            if(value === this[i]){
+                countEqual++
+            }
+        }
+        if(countEqual === this.length){
+            return true
+        }
+    }
+    return false
+}
+
+// test with node
+// console.log([1,2,3].isEqual([1,2,3]))
+// console.log([1,2,3].isEqual([2,3]))
